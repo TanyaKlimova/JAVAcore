b86bda3a86b0195972c692964163810c60eef562
Tanya <Tanya@localhost.localdomain>
2021-09-28 16:49:03 +0300

    HW3

---
+++ src/Lection03/Fruit.java
@@ -0,0 +1,13 @@
+package Lection03;
+
+abstract class Fruit {
+    private float weight;
+
+    Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
